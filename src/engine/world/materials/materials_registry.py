3b67fa7d5ae5eaff6eb04be3c3e56cf6bc05570c
AchigaLoko <oficial.aamikami.amk@gmail.com>
2026-01-30 23:37:15 -0300

    feat: created a new structure for materials

---
+++ src/engine/world/materials/materials_registry.py
@@ -0,0 +1,8 @@
+"""
+Docstring for src.engine.world.materials.materials_registry
+
+Módulo para registrar e gerenciar materiais no mundo procedural.
+
+Pega da pasta materials e extrai os conteúdos no formato yaml 
+para facilitar o acesso e a manipulação dos materiais.
+"""
